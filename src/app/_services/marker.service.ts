e0f0ca4d281ebda408b82144d9d56f7b9d91b2c9
edhems <50317004+edhems@users.noreply.github.com>
2020-08-26 14:57:47 +0200

    basic filtering gunctionality via loops comapring dates

--- src/app/_services/marker.service.ts
+++ src/app/_services/marker.service.ts
@@ -3,36 +3,46 @@ import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
 import { PopUpService } from './pop-up.service';
 import 'leaflet.markercluster';
+import * as moment from 'moment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MarkerService {
   // add &maxFeatures=50 to URL to limit number of features
-  events: string = 'http://human.zgis.at/geoserver/music_map/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=music_map:salzburg_info_api&outputFormat=application%2Fjson&maxFeatures=50';
+  events: string =
+    'http://human.zgis.at/geoserver/music_map/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=music_map:salzburg_info_api&outputFormat=application%2Fjson';
 
-  constructor(private http: HttpClient,
-          private popupService: PopUpService) {  }
+  constructor(private http: HttpClient, private popupService: PopUpService) {}
 
   makeEventMarkers(map: L.map): void {
     var markerCount = 0;
+    var date;
+    var today = moment().format('YYYY-MM-DD');
+
     let cluster = L.markerClusterGroup({
-      showCoverageOnHover: false
+      showCoverageOnHover: false,
     });
-    console.log('Creating markers...')
+    console.log('Creating markers...');
     this.http.get(this.events).subscribe((res: any) => {
-        for (const c of res.features) {
+      for (const c of res.features) {
+        const datesJson = JSON.parse(c.properties.dates);
+        for (let i = 0; i < datesJson.length; i++) {
+          date = datesJson[i];
+        }
+        console.log(date.from + 'today is ' + today);
+        if (date.to > moment().format('YYYY-MM-DD')) {
           const lat = c.geometry.coordinates[0];
           const lon = c.geometry.coordinates[1];
-          const marker = L.marker([lon, lat])
-          marker.bindPopup(this.popupService.makeEventPopup(c));     
+          const marker = L.marker([lon, lat]);
+
+          marker.bindPopup(this.popupService.makeEventPopup(c));
           cluster.addLayer(marker);
           markerCount++;
-          
+        }
+      }
+      console.log('Created ' + markerCount + ' markers');
+    });
+    map.addLayer(cluster);
   }
-  console.log('Created ' + markerCount + ' markers')
-});
-  map.addLayer(cluster);
-  
-}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
@@ -23,7 +24,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
   declarations: [
     AppComponent,
     MapComponent,
-    ToolbarComponent,  
+    ToolbarComponent,
+    FooterComponent,  
   ],
   imports: [
     BrowserModule,
