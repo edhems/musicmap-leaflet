1eb38bec60930de8b272b5738cd80cdb5a6f801c
Beckmann Max <b1079709@is.sbg.ac.at>
2020-10-22 13:11:57 +0200

    Added a new Heatmap layer made possible using the leaflet.heat plugin. For installation use npm install leaflet.heat

--- src/app/_services/marker.service.ts
+++ src/app/_services/marker.service.ts
@@ -6,6 +6,8 @@ import 'leaflet.markercluster';
 import * as moment from 'moment';
 import 'leaflet-extra-markers/dist/css/leaflet.extra-markers.min.css';
 import 'leaflet-extra-markers/dist/js/leaflet.extra-markers.js';
+import 'leaflet.heat';
+import { MapComponent } from '../map/map.component';
 
 @Injectable({
   providedIn: 'root',
@@ -19,10 +21,67 @@ export class MarkerService {
     'http://human.zgis.at/geoserver/music_map/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=music_map:musicmap_organizers&outputFormat=application%2Fjson';
 
   constructor(private http: HttpClient, private popupService: PopUpService) {}
+  // Layer Groups for organization and seperate displaying.
   organizers_grp = L.layerGroup();
   events_grp = L.layerGroup();
+  heat_grp = L.layerGroup();
   jsonTest = L.geoJson();
   isRunning: boolean = true;
+
+  ///Function to mak a Heatmap form the events. Uses leaflet.heat plugin for the heatmap.
+  makeHeatMap(map: L.map): void {
+    
+        var markerCount = 0;
+        var date;
+        var today = moment().format('YYYY-MM-DD');
+        // Mother array wich will store as many array's as there are uptodate-events.
+        let myVar = [];
+        
+        let cluster = L.markerClusterGroup ({
+         
+        });
+    
+        console.log('Creating markers...');
+        this.http.get(this.events).subscribe((res: any) => {
+          this.jsonTest = res;
+          for (const c of res.features) {
+            const datesJson = JSON.parse(c.properties.dates);
+            for (let i = 0; i < datesJson.length; i++) {
+              date = datesJson[i];
+            }
+            // If the event hasent happend yet it will be considerd for heatmap generation.
+            if (date.to > today) {
+              markerCount++;
+              // Getting the lat and lon
+              const lat = c.geometry.coordinates[0];
+              const lon = c.geometry.coordinates[1];
+              // Using another list to store each coordinate pair seperate.
+              let innerlist = [lon,lat,0.4];
+              // Push each of the above created arrays into another array, creating an array of arrays 
+              // myVar looking like : [[..][..][..]]
+              myVar.push(innerlist);
+            } else {
+              console.log('event is in the past');
+            }
+            for (let i = 0; i<markerCount; i++){
+            }
+          }
+          console.log('Created ' + markerCount + ' heat points');
+          this.isRunning = false;
+        });
+        // Creating the actuall heatmap using the L.heatLayer function. Data intake should be of format: [[lon,lat,intesity][lon,lat,intesity][..]]
+        // styling of the heatmap gradient can be done after the coordinates. For help visit https://github.com/Leaflet/Leaflet.heat 
+        var heat = L.heatLayer(myVar,
+              {radius: 25,
+              minOpacity: 0.4,
+              maxZoom: 19,
+              0: 'blue',
+              0.65: 'lime',
+              1: 'red'
+              }).addTo(cluster);
+        // Adding the cluster to a groupedLayer.
+        this.heat_grp.addLayer(cluster);
+  }
 
   makeEventMarkers(map: L.map): void {
     const popupOptions = {
